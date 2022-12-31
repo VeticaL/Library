65b1853ef10e9b5cba49dd0a9927ac3b7dd20c6a
Vertical <ambintasso@gmail.com>
2022-12-31 19:28:54 +0530

    read and remove

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
-let mylibrary = [];
+const form = document.getElementById("form");
+const titleTxt = document.getElementById("title");
+const authorTxt = document.getElementById("author");
+const pagesTxt = document.getElementById("pages");
+const readBtn = document.getElementById("readBtn");
+const removeBtn = document.getElementById("removeBtn");
+const cards = document.getElementById("cards");
 
 function Book(title, author, pages, read) {
- this.title = title
- this.author = author
- this.pages = pages
- this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookRoLibrary(title, author, pages, read) {
-  let book = new Book(title, author, pages, read)
-  mylibrary.push(book)
+function addBooktoLibrary(title, author, pages, read) {
+  title = document.getElementById("Book").value;
+  author = document.getElementById("Author").value;
+  pages = document.getElementById("Pages").value;
+  read = document.getElementById("read").value;
+
+  let book = new Book(title, author, pages, read);
+  mylibrary.push(book);
+  titleTxt.innerHTML = title;
+  authorTxt.innerHTML = author;
+  pagesTxt.innerHTML = pages;
+
+  console.log(book)
+  event.preventDefault();
 }
 
-
-const fname = document.querySelector("#book")
-const writer = document.querySelector("#Author")
-const page = document.querySelector("#Pages")
-
-function value() {
-    let Name = fname.value
-    let Writer = writer.value
-    console.log(Name, Writer)
+function toggleRead() {
+  if(read === "on") {
+    read = "off"
+    readBtn.style.backgroundColor = "red";
+  } else {
+    read = "on"
+    readBtn.style.backgroundColor = "green";
+  }
 }
 
+function remove() {
+  cards.remove();
+}
+
+form.addEventListener("submit", addBooktoLibrary);
+readBtn.addEventListener("click", toggleRead);
+removeBtn.addEventListener("click", remove);
 
 
-let book = new Book("yp", "djaj" , "ajkdl", 1)
-console.log(book)
+
